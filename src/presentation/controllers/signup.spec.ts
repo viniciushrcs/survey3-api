a5d7b723b9bbf125cfca61d823e9c0f5dc41c40f
viniciushrcs <viniciushrcs@gmail.com>
2022-10-02 16:23:09 -0300

    test: ensure SignUpController returns error 400 if no name is provided

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,19 @@
+import { SignupController } from './signup';
+
+describe('SignUpController', () => {
+  test('Should return 400 if no name is provided', () => {
+    // sut => System Under Test
+    const errorCode = 400;
+    const sut = new SignupController();
+    const httpRequest = {
+      body: {
+        email: 'email@email.com',
+        password: 'password',
+        passwordConfirmation: 'password'
+      }
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(errorCode);
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'));
+  });
+});

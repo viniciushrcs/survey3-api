8515e58820f4ce40d82b9bc570d1ce20f9798ea7
viniciushrcs <viniciushrcs@gmail.com>
2022-10-02 16:54:24 -0300

    refactor: add protocol to Controller and add a helper for badRequest

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,20 +1,20 @@
 import { HttpRequest, HttpResponse } from '../protocols/http';
 import { MissingParamError } from '../errors/missing-param-error';
+import { badRequest } from '../helpers/http-helper';
+import { Controller } from '../protocols/controller';
 
-export class SignupController {
+export class SignupController implements Controller {
   handle(httpRequest: HttpRequest): HttpResponse {
-    const { name, email } = httpRequest.body;
-    if (!name) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('name')
-      };
-    }
-    if (!email) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('email')
-      };
+    const requiredFields = [
+      'name',
+      'email',
+      'password',
+      'passwordConfirmation'
+    ];
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field));
+      }
     }
   }
 }

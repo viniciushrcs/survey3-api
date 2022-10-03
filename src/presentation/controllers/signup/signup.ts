ad34860786fd502c37f438d17e8c778265d57655
viniciushrcs <viniciushrcs@gmail.com>
2022-10-03 17:01:45 -0300

    refactor: moves success http response to a helper method

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -6,7 +6,7 @@ import {
   HttpResponse
 } from './signup-protocols';
 import { InvalidParamError, MissingParamError } from '../../errors';
-import { badRequest, serverError } from '../../helpers/http-helper';
+import { badRequest, ok, serverError } from '../../helpers/http-helper';
 
 export class SignupController implements Controller {
   private readonly emailValidator: EmailValidator;
@@ -46,10 +46,7 @@ export class SignupController implements Controller {
         password,
         email
       });
-      return {
-        statusCode: 200,
-        body: newAccount
-      };
+      return ok(newAccount);
     } catch (e) {
       // o erro capturado no catch poderia ser logado em um serviço de monitoramento
       // não é uma boa prática retornar para o cliente

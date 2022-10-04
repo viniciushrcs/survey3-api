e29aa8648e06dc5eb944705ae7d8bebd8beb8046
viniciushrcs <viniciushrcs@gmail.com>
2022-10-04 06:25:39 -0300

    feat: add addAccountRepository dependency in DbAddAccount

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -2,16 +2,26 @@ import {
   AccountModel,
   AddAccount,
   AddAccountModel,
+  AddAccountRepository,
   Encrypter
 } from './db-add-account-protocols';
 
 export class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter;
-  constructor(encrypter: Encrypter) {
+  private readonly addAccountRepository: AddAccountRepository;
+  constructor(
+    encrypter: Encrypter,
+    addAccountRepository: AddAccountRepository
+  ) {
     this.encrypter = encrypter;
+    this.addAccountRepository = addAccountRepository;
   }
   async add(account: AddAccountModel): Promise<AccountModel> {
     const encryptedPassword = await this.encrypter.encrypt(account.password);
-    return;
+    const newAccount = await this.addAccountRepository.add({
+      ...account,
+      password: encryptedPassword
+    });
+    return newAccount;
   }
 }

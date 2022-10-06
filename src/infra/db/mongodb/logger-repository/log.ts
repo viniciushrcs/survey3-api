1a95b07780be07cc70e55e894253f72bcb436132
viniciushrcs <viniciushrcs@gmail.com>
2022-10-06 09:34:53 -0300

    refactor: change date format

--- src/infra/db/mongodb/logger-repository/log.ts
+++ src/infra/db/mongodb/logger-repository/log.ts
@@ -4,6 +4,6 @@ import { MongoHelper } from '../helpers/mongo-helper';
 export class LogMongoRepository implements LogErrorRepository {
   async logError(stack: string): Promise<void> {
     const errorCollection = MongoHelper.getCollection('error');
-    await errorCollection.insertOne({ stack, date: Date.now() });
+    await errorCollection.insertOne({ stack, date: new Date() });
   }
 }

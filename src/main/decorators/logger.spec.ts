21242fd78f96a81d5ba4246a5ac7209a569c6c58
viniciushrcs <viniciushrcs@gmail.com>
2022-10-06 07:18:24 -0300

    test: ensure LoggerDecorator calls handle method of controller properly

---
+++ src/main/decorators/logger.spec.ts
@@ -0,0 +1,70 @@
+import { LoggerDecorator } from './logger';
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse
+} from '../../presentation/protocols';
+
+interface SutTypes {
+  sut: LoggerDecorator;
+  controllerStub: Controller;
+}
+
+const makeControllerStub = () => {
+  class ControllerStub implements Controller {
+    handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+      return new Promise((resolve) =>
+        resolve({
+          statusCode: 200,
+          body: {
+            email: 'email@email.com',
+            name: 'name',
+            password: 'password'
+          }
+        })
+      );
+    }
+  }
+  return new ControllerStub();
+};
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeControllerStub();
+  const sut = new LoggerDecorator(controllerStub);
+  return {
+    sut,
+    controllerStub
+  };
+};
+
+describe('LoggerDeccorator', () => {
+  test('Should call controller handle properly', async () => {
+    const { sut, controllerStub } = makeSut();
+    const httpRequest: HttpRequest = {
+      body: {
+        email: 'email@email.com',
+        name: 'name',
+        password: 'password',
+        passwordConfirmation: 'password'
+      }
+    };
+    const handleSpy = jest.spyOn(controllerStub, 'handle');
+    await sut.handle(httpRequest);
+    expect(handleSpy).toHaveBeenCalled();
+  });
+
+  test('Should call controller handle with correct values', async () => {
+    const { sut, controllerStub } = makeSut();
+    const httpRequest: HttpRequest = {
+      body: {
+        email: 'email@email.com',
+        name: 'name',
+        password: 'password',
+        passwordConfirmation: 'password'
+      }
+    };
+    const handleSpy = jest.spyOn(controllerStub, 'handle');
+    await sut.handle(httpRequest);
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest);
+  });
+});

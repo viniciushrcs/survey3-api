2ab71348e43375ab2c7deb1bd0f58885510da228
viniciushrcs <viniciushrcs@gmail.com>
2022-10-06 17:16:03 -0300

    test: ensure LoginController returns 400 if no email is provided

---
+++ src/presentation/controllers/login/login.spec.ts
@@ -0,0 +1,21 @@
+import { HttpRequest } from '../../protocols';
+import { badRequest } from '../../helpers/http-helper';
+import { MissingParamError } from '../../errors';
+import { LoginController } from './login';
+
+describe('Login Controller', () => {
+  const makeSut = () => {
+    return new LoginController();
+  };
+
+  test('Should return 400 if no email is provided', async () => {
+    const sut = makeSut();
+    const httpRequest: HttpRequest = {
+      body: {
+        password: 'password'
+      }
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('email')));
+  });
+});

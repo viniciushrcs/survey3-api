aea77ac4f05ddf0817d90732fc2b825ccc23d3ad
viniciushrcs <viniciushrcs@gmail.com>
2022-10-07 07:56:22 -0300

    refactor: change signup factories to a single folder

--- src/main/factories/signup/signup.ts
+++ src/main/factories/signup/signup.ts
@@ -1,10 +1,10 @@
-import { SignupController } from '../../presentation/controllers/signup/signup';
-import { DbAddAccount } from '../../data/usecases/add-account/db-add-account';
-import { BcryptAdapter } from '../../infra/crypto/bcrypt-adapter';
-import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account';
-import { LogErrorDecorator } from '../decorators/log-error';
-import { LogMongoRepository } from '../../infra/db/mongodb/logger-repository/log';
-import { Controller } from '../../presentation/protocols';
+import { SignupController } from '../../../presentation/controllers/signup/signup';
+import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account';
+import { BcryptAdapter } from '../../../infra/crypto/bcrypt-adapter';
+import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account';
+import { LogErrorDecorator } from '../../decorators/log-error';
+import { LogMongoRepository } from '../../../infra/db/mongodb/logger-repository/log';
+import { Controller } from '../../../presentation/protocols';
 import { makeSignUpValidation } from './signup-validation';
 
 export const makeSignUpController = (): Controller => {

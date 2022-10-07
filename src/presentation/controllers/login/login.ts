c6786894a07c6de057f8f40f185e0bf5073c0777
viniciushrcs <viniciushrcs@gmail.com>
2022-10-07 07:49:07 -0300

    feat: add Validation to LoginController

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -4,33 +4,27 @@ import {
   serverError,
   unauthorized
 } from '../../helpers/http-helper';
-import { InvalidParamError, MissingParamError } from '../../errors';
 import {
   Authentication,
   Controller,
-  EmailValidator,
   HttpRequest,
-  HttpResponse
+  HttpResponse,
+  Validation
 } from './login-protocols';
 
 export class LoginController implements Controller {
   constructor(
-    private readonly emailValidator: EmailValidator,
+    private readonly validation: Validation,
     private readonly authentication: Authentication
   ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const { password, email } = httpRequest.body;
     try {
-      const requiredFields = ['email', 'password'];
-      for (const field of requiredFields) {
-        if (!httpRequest.body[field]) {
-          return badRequest(new MissingParamError(field));
-        }
+      const validationError = this.validation.validate(httpRequest.body);
+      if (validationError) {
+        return badRequest(validationError);
       }
-      const isEmailValid = this.emailValidator.isValid(email);
-      if (!isEmailValid) {
-        return badRequest(new InvalidParamError('email'));
-      }
+
       const authToken = await this.authentication.authenticate(email, password);
       if (!authToken) {
         return unauthorized();

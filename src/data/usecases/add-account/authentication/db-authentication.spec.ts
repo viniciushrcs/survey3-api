f9133bc61911b07f4f87ad75ed4cdd04cfea1283
viniciushrcs <viniciushrcs@gmail.com>
2022-10-08 06:41:29 -0300

    refactor: move objects creation to a factory method

--- src/data/usecases/add-account/authentication/db-authentication.spec.ts
+++ src/data/usecases/add-account/authentication/db-authentication.spec.ts
@@ -1,37 +1,44 @@
 import { DbAuthentication } from './db-authentication';
 import { LoadAccountByEmailRepository } from '../../../protocols/load-account-by-email-repository.ts';
 import { AccountModel } from '../../../../domain/models/account';
+import { AuthenticationModel } from '../../../../domain/usecases/authentication';
+
+interface SutTypes {
+  loadAccountByEmailRepoStub: LoadAccountByEmailRepository;
+  sut: DbAuthentication;
+}
+
+const makeFakeAccount = (): AccountModel => ({
+  id: 'c53cd3f2-df40-497a-8061-56d9f8d4df01',
+  name: 'name',
+  email: 'email@email.com',
+  password: 'wTzVTHQ6'
+});
+
+const makeFakeAuthInput = (): AuthenticationModel => ({
+  email: 'email@email.com',
+  password: 'password'
+});
+
+const makeLoadAccountByEmailRepoStub = () => {
+  class LoadAccountByEmailRepoStub implements LoadAccountByEmailRepository {
+    async loadAccount(email: string): Promise<AccountModel> {
+      return new Promise((resolve) => resolve(makeFakeAccount()));
+    }
+  }
+  return new LoadAccountByEmailRepoStub();
+};
+
+const makeSut = (): SutTypes => {
+  const loadAccountByEmailRepoStub = makeLoadAccountByEmailRepoStub();
+  const sut = new DbAuthentication(loadAccountByEmailRepoStub);
+  return {
+    sut,
+    loadAccountByEmailRepoStub
+  };
+};
 
 describe('DbAuthentication', () => {
-  interface SutTypes {
-    loadAccountByEmailRepoStub: LoadAccountByEmailRepository;
-    sut: DbAuthentication;
-  }
-
-  const makeLoadAccountByEmailRepoStub = () => {
-    class LoadAccountByEmailRepoStub implements LoadAccountByEmailRepository {
-      async loadAccount(email: string): Promise<AccountModel> {
-        const account: AccountModel = {
-          id: 'c53cd3f2-df40-497a-8061-56d9f8d4df01',
-          name: 'name',
-          email: 'email@email.com',
-          password: 'wTzVTHQ6'
-        };
-        return new Promise((resolve) => resolve(account));
-      }
-    }
-    return new LoadAccountByEmailRepoStub();
-  };
-
-  const makeSut = (): SutTypes => {
-    const loadAccountByEmailRepoStub = makeLoadAccountByEmailRepoStub();
-    const sut = new DbAuthentication(loadAccountByEmailRepoStub);
-    return {
-      sut,
-      loadAccountByEmailRepoStub
-    };
-  };
-
   describe('LoadAccountByEmailRepository', () => {
     test(' Should call LoadAccountByEmailRepository with correct email', async () => {
       const { sut, loadAccountByEmailRepoStub } = makeSut();
@@ -39,10 +46,7 @@ describe('DbAuthentication', () => {
         loadAccountByEmailRepoStub,
         'loadAccount'
       );
-      await sut.authenticate({
-        email: 'email@email.com',
-        password: 'password'
-      });
+      await sut.authenticate(makeFakeAuthInput());
       expect(loadAccountSpy).toHaveBeenCalledWith('email@email.com');
     });
   });

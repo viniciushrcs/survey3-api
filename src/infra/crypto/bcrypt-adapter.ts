1d85dbad3bf4ebd750e57f568b9aa9085107a001
viniciushrcs <viniciushrcs@gmail.com>
2022-10-08 08:19:24 -0300

    feat: ensure BCryptAdapter calls Bcrypt.compare properly

--- src/infra/crypto/bcrypt-adapter.ts
+++ src/infra/crypto/bcrypt-adapter.ts
@@ -1,21 +1,18 @@
-// Única classe que se suja com o bcrypt
-// O padrão adapter faz com que apenas uma classe se suje com uma dependência externa
-// e ela tem que se adaptar para se plugar ao seu sistema
-// o adapter faz a adaptação mundo externo -> interface -> mundo interno
 import { Encrypter } from '../../data/protocols/crypto/encrypter';
-import { hash } from 'bcrypt';
+import { compare, hash } from 'bcrypt';
+import { HashComparer } from '../../data/protocols/crypto/hash-comparer';
 
-export class BcryptAdapter implements Encrypter {
+export class BcryptAdapter implements Encrypter, HashComparer {
   private readonly salt: number;
 
   constructor(salt: number) {
     this.salt = salt;
-    // já que o salt é específico dessa implementação, a gente não suja
-    // o método encrypt mexendo na interface
-    // mantemos o protocolo como está e as dependências dessa implementação
-    // vêm no constructor
   }
   async encrypt(value: string): Promise<string> {
     return await hash(value, this.salt);
+  }
+
+  async compare(value: string, hash: string): Promise<boolean> {
+    return await compare(value, hash);
   }
 }

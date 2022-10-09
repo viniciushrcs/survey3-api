c7da778a0f2db230abeb7b9053d24d2d6ae2fa24
viniciushrcs <viniciushrcs@gmail.com>
2022-10-09 13:31:31 -0300

    feat: ensure Auth Middleware return 200 if LoadAccountByToken succeeds

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,5 +1,5 @@
 import { HttpRequest, HttpResponse, Middleware } from '../protocols';
-import { forbidden } from '../helpers/http/http-helper';
+import { forbidden, ok } from '../helpers/http/http-helper';
 import { AccessDeniedError } from '../errors';
 import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token';
 
@@ -7,9 +7,16 @@ export class AuthMiddleware implements Middleware {
   constructor(private readonly loadAccountByToken: LoadAccountByToken) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const accessToken = httpRequest.headers?.['x-access-token'];
+    let account = null;
+
     if (accessToken) {
-      await this.loadAccountByToken.loadAccount(accessToken);
+      account = await this.loadAccountByToken.loadAccount(accessToken);
     }
+
+    if (account) {
+      return ok({ accountId: account.id });
+    }
+
     return forbidden(new AccessDeniedError());
   }
 }

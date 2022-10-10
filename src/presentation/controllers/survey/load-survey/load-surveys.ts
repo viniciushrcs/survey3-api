d2f48d9ddb386cc7822925a58a5c6aed75a4589d
viniciushrcs <viniciushrcs@gmail.com>
2022-10-10 06:30:35 -0300

    feat: ensure Load Surveys Controller return 204 if there isn't a survey

--- src/presentation/controllers/survey/load-survey/load-surveys.ts
+++ src/presentation/controllers/survey/load-survey/load-surveys.ts
@@ -4,14 +4,14 @@ import {
   HttpResponse,
   LoadSurveys
 } from './load-surveys-protocols';
-import { ok, serverError } from '../../../helpers/http/http-helper';
+import { noContent, ok, serverError } from '../../../helpers/http/http-helper';
 
 export class LoadSurveysController implements Controller {
   constructor(private readonly loadSurveys: LoadSurveys) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const surveys = await this.loadSurveys.load();
-      return ok(surveys);
+      return surveys.length ? ok(surveys) : noContent();
     } catch (e) {
       return serverError(e);
     }

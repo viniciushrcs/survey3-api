521a74f609189f5f3c2341a37b2092b6d17a5370
viniciushrcs <viniciushrcs@gmail.com>
2022-10-10 06:08:51 -0300

    test: ensure Load Surveys Controller returns 500 if LoadSurveys throws

--- src/presentation/controllers/survey/load-survey/load-surveys.spec.ts
+++ src/presentation/controllers/survey/load-survey/load-surveys.spec.ts
@@ -1,7 +1,8 @@
 import { LoadSurveysController } from './load-surveys';
 import { LoadSurveys, SurveyModel } from './load-surveys-protocols';
-import { ok } from '../../../helpers/http/http-helper';
+import { ok, serverError } from '../../../helpers/http/http-helper';
 import MockDate from 'mockdate';
+import { ServerError } from '../../../errors';
 
 const makeFakeSurveys = (): SurveyModel[] => [
   {
@@ -60,16 +61,27 @@ describe('Load Surveys Controller', () => {
     };
   };
 
-  test('Should call LoadSurveys', async () => {
-    const { sut, loadSurveysStub } = makeSut();
-    const loadSpy = jest.spyOn(loadSurveysStub, 'load');
-    await sut.handle({});
-    expect(loadSpy).toHaveBeenCalled();
-  });
+  describe('LoadSurveys', () => {
+    test('Should call LoadSurveys', async () => {
+      const { sut, loadSurveysStub } = makeSut();
+      const loadSpy = jest.spyOn(loadSurveysStub, 'load');
+      await sut.handle({});
+      expect(loadSpy).toHaveBeenCalled();
+    });
 
-  test('Should return 200 on success', async () => {
-    const { sut } = makeSut();
-    const httpResponse = await sut.handle({});
-    expect(httpResponse).toEqual(ok(makeFakeSurveys()));
+    test('Should return 500 if LoadSurveys throws', async () => {
+      const { sut, loadSurveysStub } = makeSut();
+      jest.spyOn(loadSurveysStub, 'load').mockRejectedValueOnce(async () => {
+        throw new Error();
+      });
+      const httpResponse = await sut.handle({});
+      expect(httpResponse).toEqual(serverError(new ServerError(null)));
+    });
+
+    test('Should return 200 on success', async () => {
+      const { sut } = makeSut();
+      const httpResponse = await sut.handle({});
+      expect(httpResponse).toEqual(ok(makeFakeSurveys()));
+    });
   });
 });

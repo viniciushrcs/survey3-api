2a7e4e9a8cab0fd6c196cffe7a55a8ea5ddabc85
viniciushrcs <viniciushrcs@gmail.com>
2022-10-10 05:55:57 -0300

    feat: ensure Load Surveys Controller call LoadSurveys properly

---
+++ src/presentation/controllers/survey/load-survey/load-surveys.ts
@@ -0,0 +1,14 @@
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse,
+  LoadSurveys
+} from './load-surveys-protocols';
+
+export class LoadSurveysController implements Controller {
+  constructor(private readonly loadSurveys: LoadSurveys) {}
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    await this.loadSurveys.load();
+    return null;
+  }
+}

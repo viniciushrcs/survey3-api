5ac3b7185eae18e623f2ed4fdd692b5895ec15fc
viniciushrcs <viniciushrcs@gmail.com>
2022-10-10 07:30:47 -0300

    test: ensure GET /surveys returns 403, 204 and 200

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -6,6 +6,7 @@ import { sign } from 'jsonwebtoken';
 import env from '../config/env';
 
 let accountCollection: Collection;
+let surveyCollection: Collection;
 describe('Survey Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL);
@@ -16,7 +17,7 @@ describe('Survey Routes', () => {
   });
 
   beforeEach(async () => {
-    const surveyCollection: Collection = MongoHelper.getCollection('surveys');
+    surveyCollection = MongoHelper.getCollection('surveys');
     await surveyCollection.deleteMany({});
     accountCollection = MongoHelper.getCollection('accounts');
     await accountCollection.deleteMany({});
@@ -76,6 +77,86 @@ describe('Survey Routes', () => {
           ]
         })
         .expect(204);
+    });
+  });
+
+  describe('GET /surveys', () => {
+    test('Should return 403 on survey without accessToken', async () => {
+      await request(app).get('/api/surveys').send().expect(403);
+    });
+
+    test('Should return 204 on survey with accessToken and without surveys saved', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'name',
+        email: 'email@email.com',
+        password: '123',
+        role: 'admin'
+      });
+      const id = res.insertedId;
+      const accessToken = sign({ id }, env.secret);
+      await accountCollection.updateOne(
+        {
+          name: 'name'
+        },
+        {
+          $set: {
+            accessToken
+          }
+        }
+      );
+      await request(app)
+        .get('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({})
+        .expect(204);
+    });
+
+    test('Should return 200 on survey with accessToken and surveys saved', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'name',
+        email: 'email@email.com',
+        password: '123',
+        role: 'admin'
+      });
+      const id = res.insertedId;
+      const accessToken = sign({ id }, env.secret);
+      await accountCollection.updateOne(
+        {
+          name: 'name'
+        },
+        {
+          $set: {
+            accessToken
+          }
+        }
+      );
+      await surveyCollection.insertMany([
+        {
+          question: 'any_question',
+          answers: [
+            {
+              image: 'any_image',
+              answer: 'any_answer'
+            }
+          ],
+          date: new Date()
+        },
+        {
+          question: 'other_question',
+          answers: [
+            {
+              image: 'other_image',
+              answer: 'other_answer'
+            }
+          ],
+          date: new Date()
+        }
+      ]);
+      await request(app)
+        .get('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({})
+        .expect(200);
     });
   });
 });

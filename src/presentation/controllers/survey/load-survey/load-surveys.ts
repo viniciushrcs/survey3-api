9ce651284ce65c40ace1b0e3477d6342288ecca8
viniciushrcs <viniciushrcs@gmail.com>
2022-10-10 06:05:41 -0300

    feat: ensure Load Surveys Controller returns 200 on LoadSurveys success

--- src/presentation/controllers/survey/load-survey/load-surveys.ts
+++ src/presentation/controllers/survey/load-survey/load-surveys.ts
@@ -4,11 +4,12 @@ import {
   HttpResponse,
   LoadSurveys
 } from './load-surveys-protocols';
+import { ok } from '../../../helpers/http/http-helper';
 
 export class LoadSurveysController implements Controller {
   constructor(private readonly loadSurveys: LoadSurveys) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadSurveys.load();
-    return null;
+    const surveys = await this.loadSurveys.load();
+    return ok(surveys);
   }
 }

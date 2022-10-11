a2cc5f72d72b07d8b35fa74f5085b844853ac59e
viniciushrcs <viniciushrcs@gmail.com>
2022-10-11 06:13:21 -0300

    test: ensure DbSaveSurveyResult integration with SaveSurveyResultRepository

---
+++ src/data/usecases/survey/save-survey-result/db-save-survey-result.spec.ts
@@ -0,0 +1,72 @@
+import MockDate from 'mockdate';
+import { DbSaveSurveyResult } from './db-save-survey-result';
+import {
+  SaveSurveyResultModel,
+  SaveSurveyResultRepository,
+  SurveyResultModel
+} from './db-save-survey-result-protocols';
+
+interface SutTypes {
+  sut: DbSaveSurveyResult;
+  saveSurveyResultRepositoryStub: SaveSurveyResultRepository;
+}
+
+const makeSurveyResultModel = (): SurveyResultModel => ({
+  ...makeSaveSurveyResultModel(),
+  id: 'any_id'
+});
+
+const makeSaveSurveyResultModel = (): SaveSurveyResultModel => ({
+  surveyId: 'survey_id',
+  userId: 'user_id',
+  answer: 'any_answer',
+  date: new Date()
+});
+
+const makeSaveSurveyResultRepository = (): SaveSurveyResultRepository => {
+  class SaveSurveyResultRepositoryStub implements SaveSurveyResultRepository {
+    async save(survey: SaveSurveyResultModel): Promise<SurveyResultModel> {
+      return new Promise((resolve) => resolve(makeSurveyResultModel()));
+    }
+  }
+  return new SaveSurveyResultRepositoryStub();
+};
+
+const makeSut = (): SutTypes => {
+  const saveSurveyResultRepositoryStub = makeSaveSurveyResultRepository();
+  const sut = new DbSaveSurveyResult(saveSurveyResultRepositoryStub);
+  return {
+    sut,
+    saveSurveyResultRepositoryStub
+  };
+};
+
+describe('DbSaveSurveyResult UseCase', () => {
+  beforeAll(() => {
+    MockDate.set(new Date());
+  });
+
+  afterAll(() => {
+    MockDate.reset;
+  });
+
+  describe('SaveSurveyResult Repository', () => {
+    test('Should call SaveSurveyResultRepository with correct values', async () => {
+      const { sut, saveSurveyResultRepositoryStub } = makeSut();
+      const addSpy = jest.spyOn(saveSurveyResultRepositoryStub, 'save');
+      await sut.save(makeSaveSurveyResultModel());
+      expect(addSpy).toHaveBeenCalledWith(makeSaveSurveyResultModel());
+    });
+
+    test('Should throw if SaveSurveyResultRepository throws', async () => {
+      const { sut, saveSurveyResultRepositoryStub } = makeSut();
+      jest
+        .spyOn(saveSurveyResultRepositoryStub, 'save')
+        .mockRejectedValueOnce(() => {
+          throw new Error();
+        });
+      const promise = sut.save(makeSaveSurveyResultModel());
+      await expect(promise).rejects.toThrow();
+    });
+  });
+});

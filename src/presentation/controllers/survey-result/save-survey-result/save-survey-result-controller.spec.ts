4882f0828fe24eec5618be37ffd787008ab2ed93
viniciushrcs <viniciushrcs@gmail.com>
2022-10-11 20:09:20 -0300

    test: ensure SaveSurveyResult Controller returns 500 if SaveSurveyResult throws

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
@@ -143,5 +143,16 @@ describe('SaveSurveyResult Controller', () => {
         date: new Date()
       });
     });
+
+    test('Should return 500 if SaveSurveyResult throws', async () => {
+      const { sut, saveSurveyResultStub } = makeSut();
+      jest
+        .spyOn(saveSurveyResultStub, 'save')
+        .mockRejectedValueOnce(async () => {
+          throw new Error();
+        });
+      const httpResponse = await sut.handle(makeFakeRequest());
+      expect(httpResponse).toEqual(serverError(new ServerError(null)));
+    });
   });
 });

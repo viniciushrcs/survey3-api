4c88474d8f0201f37450c7c85db3d8b543713ad6
viniciushrcs <viniciushrcs@gmail.com>
2022-10-11 05:44:03 -0300

    feat: ensure DbLoadSurveyById integration with LoadSurveyByIdRepository

---
+++ src/data/usecases/survey/load-survey-by-id/db-load-survey-by-id-protocols.ts
@@ -0,0 +1,3 @@
+export * from '../../../../domain/usecases/load-survey-by-id';
+export * from '../../../../domain/models/survey';
+export * from '../../../protocols/db/survey/load-survey-by-id-repository';

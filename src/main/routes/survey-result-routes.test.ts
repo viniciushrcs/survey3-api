07c186ed0cd7999b680c7b19a86f111e110584f8
viniciushrcs <viniciushrcs@gmail.com>
2022-10-12 06:35:37 -0300

    test: ensure PUT /surveys/:surveyId/results returns 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -2,10 +2,12 @@ import request from 'supertest';
 import app from '../config/app';
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
 import { Collection } from 'mongodb';
+import { sign } from 'jsonwebtoken';
+import env from '../config/env';
 
 let accountCollection: Collection;
 let surveyCollection: Collection;
-describe('Survey Routes', () => {
+describe('SurveyResult Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL);
   });
@@ -26,9 +28,50 @@ describe('Survey Routes', () => {
       await request(app)
         .put('/api/surveys/any_id/results')
         .send({
-          answer: 'any_answer_2'
+          answer: 'any_answer'
         })
         .expect(403);
+    });
+
+    test('Should return 200 on save survey result with accessToken', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'name',
+        email: 'email@email.com',
+        password: '123',
+        role: 'admin'
+      });
+      const id = res.insertedId;
+      const accessToken = sign({ id }, env.secret);
+      await accountCollection.updateOne(
+        {
+          name: 'name'
+        },
+        {
+          $set: {
+            accessToken
+          }
+        }
+      );
+      const survey = await surveyCollection.insertOne({
+        question: 'any_question',
+        answers: [
+          {
+            image: 'any_image',
+            answer: 'any_answer'
+          },
+          {
+            answer: 'answer_2'
+          }
+        ],
+        date: new Date()
+      });
+      await request(app)
+        .put(`/api/surveys/${survey.insertedId.toString()}/results`)
+        .set('x-access-token', accessToken)
+        .send({
+          answer: 'any_answer'
+        })
+        .expect(200);
     });
   });
 });

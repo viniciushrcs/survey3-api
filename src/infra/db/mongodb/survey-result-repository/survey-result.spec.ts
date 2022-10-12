d2829580317d583d6ef59f15c6f682b8eaa499ee
viniciushrcs <viniciushrcs@gmail.com>
2022-10-12 15:07:15 -0300

    test: ensure SurveyResultMongoRepository loads surveyResults correctly

--- src/infra/db/mongodb/survey-result-repository/survey-result.spec.ts
+++ src/infra/db/mongodb/survey-result-repository/survey-result.spec.ts
@@ -30,6 +30,12 @@ const makeSurvey = async (): Promise<SurveyModel> => {
       },
       {
         answer: 'any_answer_2'
+      },
+      {
+        answer: 'any_answer_3'
+      },
+      {
+        answer: 'any_answer_4'
       }
     ],
     date: new Date()
@@ -120,6 +126,51 @@ describe('SurveyResult Mongo Repository', () => {
       expect(savedSurveyResult.answers[0].percent).toBe(100);
       expect(savedSurveyResult.answers[1].count).toBe(0);
       expect(savedSurveyResult.answers[1].percent).toBe(0);
+    });
+  });
+
+  describe('loadBySurveyId', () => {
+    test('Should load survey result with random results', async () => {
+      const survey = await makeSurvey();
+      const account = await makeAccount();
+      await surveyResultCollection.insertMany([
+        {
+          surveyId: new ObjectId(survey.id),
+          accountId: new ObjectId(account.id),
+          answer: survey.answers[0].answer,
+          date: new Date()
+        },
+        {
+          surveyId: new ObjectId(survey.id),
+          accountId: new ObjectId(account.id),
+          answer: survey.answers[0].answer,
+          date: new Date()
+        },
+        {
+          surveyId: new ObjectId(survey.id),
+          accountId: new ObjectId(account.id),
+          answer: survey.answers[1].answer,
+          date: new Date()
+        },
+        {
+          surveyId: new ObjectId(survey.id),
+          accountId: new ObjectId(account.id),
+          answer: survey.answers[1].answer,
+          date: new Date()
+        }
+      ]);
+      const sut = makeSut();
+      const surveyResult = await sut.loadBySurveyId(survey.id);
+      expect(surveyResult).toBeTruthy();
+      expect(surveyResult.surveyId).toEqual(survey.id);
+      expect(surveyResult.answers[0].count).toBe(2);
+      expect(surveyResult.answers[0].percent).toBe(50);
+      expect(surveyResult.answers[1].count).toBe(2);
+      expect(surveyResult.answers[1].percent).toBe(50);
+      expect(surveyResult.answers[2].count).toBe(0);
+      expect(surveyResult.answers[2].percent).toBe(0);
+      expect(surveyResult.answers[3].count).toBe(0);
+      expect(surveyResult.answers[3].percent).toBe(0);
     });
   });
 });

75620b6eba2dccc15299a1ace2e06247b13fcbbe
viniciushrcs <viniciushrcs@gmail.com>
2022-10-14 05:28:45 -0300

    feat: Should call LoadSurveyByIdRepository with correct values LoadSurveyResultRepository returns null

--- src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
@@ -3,13 +3,21 @@ import {
   LoadSurveyResultRepository,
   SurveyResultModel
 } from './db-load-survey-result-protocols';
+import { LoadSurveyByIdRepository } from '../../../protocols/db/survey/load-survey-by-id-repository';
 
 export class DbLoadSurveyResult implements LoadSurveyResult {
   constructor(
-    private readonly loadSurveyResultRepository: LoadSurveyResultRepository
+    private readonly loadSurveyResultRepository: LoadSurveyResultRepository,
+    private readonly loadSurveyByIdRepository: LoadSurveyByIdRepository
   ) {}
 
   async load(surveyId: string): Promise<SurveyResultModel> {
-    return await this.loadSurveyResultRepository.loadBySurveyId(surveyId);
+    const surveyResult = await this.loadSurveyResultRepository.loadBySurveyId(
+      surveyId
+    );
+    if (!surveyResult) {
+      await this.loadSurveyByIdRepository.loadById(surveyId);
+    }
+    return surveyResult;
   }
 }
